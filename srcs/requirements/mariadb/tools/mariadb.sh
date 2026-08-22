cd910cbf04a63466ec81aa4f1e9a65be88da9fd6
Jimmy Parcero Otero <jiparcer@c2r5s1.42lausanne.ch>
2026-08-22 20:59:23 +0200

    correction22_08

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -10,13 +10,13 @@ sleep 5
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
 # crée le user si inex + mdp
-mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 
 # donne a sys user tout les droit sur la db
-mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 
 # changement mdp du root user mariadb
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 
 # application des changement
 mariadb -e "FLUSH PRIVILEGES;"
@@ -26,5 +26,5 @@ mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
 
 
 #lunch mariadb en foreground, docker s arrete si pid 1 se termine, mysql_safe permet de garder le conteneur online malgrés ca
-exec mysql_safe
+exec mysqld_safe
 
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -13,23 +13,23 @@ sleep 10
 
 #si le fichier wp-config.php est inex, alors wordpress pas encore installé
 
-if[ ! -e wp-config.php ]; then
+if [ ! -e wp-config.php ]; then
 	echo "Installation de WordPress..."
 
 	#telechargement des fichier de base de wordpress
 	wp core download --allow-root
 	
 	#creation du fichier config pour se co a mariadb
-	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbost=mariadb --allow-root
+	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root
 	#installation de wordpress + creation de l admin
-	wp core install --url=$DOMAIN_NAME --title=WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_EMAIL --alow-root
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 
 	#creation du 2eme user demandé dans sbjct
 	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 	
-	echo"WordPress est installé et configuré !"
+	echo "WordPress est installé et configuré !"
 else
-	echo"WordPress est déjà installé"
+	echo "WordPress est déjà installé"
 fi
 
 #donne les bon droit au dossier web
@@ -37,5 +37,5 @@ chown -R www-data:www-data /var/www/html
 
 #comme pour mariadb on lance PHP-FPM en foreground (PID 1) avec exec
 # -F force PHP-FPM a rester en foreground
-exec /usr/sbin/php-fpm8.2 -F
+exec /usr/sbin/php-fpm7.4 -F
 
